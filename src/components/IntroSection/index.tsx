c8d64834820e63a78f9167aed72a3ef4d7ccc7e1
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-10-30 21:17:29 -0300

    Add adjustments to IntroSection

--- src/components/IntroSection/index.tsx
+++ src/components/IntroSection/index.tsx
@@ -1,8 +1,13 @@
 import styles from './styles.module.css';
 
+import IntroBg from '../../assets/img/intro_bg.jpg'
+
 const IntroSection = () => {
     return (
-        <section id='intro' className={styles.intro_section}>
+        <section id="intro" className={styles.intro_section}>
+            <div className={styles.image_wrapper}>
+                <img src={IntroBg} alt="Imagem de introdução" className={styles.image} />
+            </div>
             <div className={styles.content}>
                 <h1 className={styles.title}>Bem-vindo à Minha Consultoria</h1>
                 <p className={styles.description}>

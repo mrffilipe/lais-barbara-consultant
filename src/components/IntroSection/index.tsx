cff0d42a53ce2a291095e36d9950e0a3345a9ea6
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-11-19 08:49:01 -0300

    Add IntroSection refactoring

--- src/components/IntroSection/index.tsx
+++ src/components/IntroSection/index.tsx
@@ -1,19 +1,16 @@
 import styles from './styles.module.css';
 
-import IntroBg from '../../assets/img/intro_bg.jpg'
-
 const IntroSection = () => {
     return (
         <section id="intro" className={styles.intro_section}>
-            <div className={styles.image_wrapper}>
-                <img src={IntroBg} alt="Imagem de introdução" className={styles.image} />
-            </div>
-            <div className={styles.content}>
-                <h1 className={styles.title}>Bem-vindo à Minha Consultoria</h1>
-                <p className={styles.description}>
-                    Aqui você encontrará soluções personalizadas para otimizar seus negócios e alcançar os melhores resultados.
-                </p>
-                <button className={styles.cta_button}>Saiba Mais</button>
+            <div className={styles.overlay}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Bem-vindo à Minha Consultoria</h1>
+                    <p className={styles.description}>
+                        Aqui você encontrará soluções personalizadas para otimizar seus negócios e alcançar os melhores resultados.
+                    </p>
+                    <a href='#intro' className={styles.cta_button}>Saiba Mais</a>
+                </div>
             </div>
         </section>
     );

43b9fe14b748fadfb8c3393900d70a89bc9f2fa6
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-11-24 22:31:51 -0300

    Add refactoring AboutSection

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -1,14 +1,17 @@
 import styles from './styles.module.css';
 
+import ProfileImg from '../../assets/img/profile.jpg';
+
 const AboutSection = () => {
     return (
         <section id='about' className={styles.about_section}>
             <div className={styles.content}>
-                <h2 className={styles.title}>Sobre Mim</h2>
-                <p className={styles.description}>
-                    Sou uma consultora dedicada a ajudar empresas a alcançarem seus objetivos por meio de estratégias eficientes e personalizadas. Com anos de experiência no mercado, eu ofereço soluções focadas em resultados e adaptadas às necessidades de cada cliente.
+                <h1>Sobre Mim</h1>
+                <p>
+                    Filha de uma professora e de um operário, sempre busquei crescimento pessoal e profissional. Sou casada e tenho como propósito compartilhar conhecimento, ajudando pessoas a se desenvolverem. Com experiência em saúde ocupacional, ergonomia e bem-estar, ofereço palestras personalizadas focadas no desenvolvimento pessoal e profissional. Ensino técnicas para gerenciar o estresse e transformar desafios em oportunidades, promovendo clareza e automotivação.
                 </p>
             </div>
+            <img className={styles.profile} src={ProfileImg} alt="Foto de Laís" draggable={false} />
         </section>
     );
 };

1f4387ad109dd71143c3309bae59085eb29f2c6e
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-12-20 14:54:33 -0300

    Add refactoring Footer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
+import { Close, Menu } from "@mui/icons-material";
 import { useState } from "react";
 
 import styles from "./styles.module.css";
 
-import LogoImg from "../../assets/img/logo.png";
+import Logo from "../Logo";
+import SocialMedia from "../SocialMedia";
 
 const Header = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -20,23 +22,30 @@ const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
-				<div className={styles.logo}>
-					<img src={LogoImg} alt="Logo" />
-				</div>
-				<button className={styles.menu_button} onClick={toggleMenu}>
-					☰
+				<Logo />
+				<button
+					className={styles.menu_button}
+					type="button"
+					onClick={toggleMenu}
+				>
+					<Menu />
 				</button>
+				{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
 				<div
 					className={`${styles.overlay} ${isMenuOpen ? styles.visible : ""}`}
 					onClick={toggleMenu}
-				></div>
+				/>
 				<nav className={`${styles.nav} ${isMenuOpen ? styles.open : ""}`}>
-					<button className={styles.close_button} onClick={toggleMenu}>
-						X
+					<button
+						className={styles.close_button}
+						type="button"
+						onClick={toggleMenu}
+					>
+						<Close />
 					</button>
 					<ul className={styles.menu_list}>
 						<li>
-							<a href="#intro">Início</a>
+							<a href="#home">Início</a>
 						</li>
 						<li>
 							<a href="#about">Sobre</a>
@@ -45,14 +54,9 @@ const Header = () => {
 							<a href="#services">Serviços</a>
 						</li>
 						<li>
-							<a href="#consulting">Consultorias</a>
-						</li>
-						<li>
-							<a href="#testimonial">Depoimentos</a>
-						</li>
-						<li>
 							<a href="#contact">Contato</a>
 						</li>
+						<SocialMedia className={styles.social_media} />
 					</ul>
 				</nav>
 			</div>

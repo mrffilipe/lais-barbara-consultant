f85f7100b242978a6bfd662a4d5ab3aa30c38a3c
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-12-20 13:57:36 -0300

    Add refactoring About

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -1,28 +1,28 @@
 import styles from "./styles.module.css";
 
-import ProfileImg from "../../assets/img/profile.jpg";
+import Profile from "../../assets/img/profile.jpeg";
 
 const AboutSection = () => {
 	return (
 		<section id="about" className={styles.about_section}>
-			<div className={styles.content}>
-				<h1>Sobre Mim</h1>
-				<p>
-					Filha de uma professora e de um operário, sempre busquei crescimento
-					pessoal e profissional. Sou casada e tenho como propósito compartilhar
-					conhecimento, ajudando pessoas a se desenvolverem. Com experiência em
-					saúde ocupacional, ergonomia e bem-estar, ofereço palestras
-					personalizadas focadas no desenvolvimento pessoal e profissional.
-					Ensino técnicas para gerenciar o estresse e transformar desafios em
-					oportunidades, promovendo clareza e automotivação.
-				</p>
+			<div className={styles.container}>
+				<div className={styles.profile}>
+					<img src={Profile} alt="Foto de Laís" draggable={false} />
+				</div>
+				<div className={styles.content}>
+					<h1>Sobre Mim</h1>
+					<p>
+						Filha de uma professora e de um operário, sempre busquei crescimento
+						pessoal e profissional. Sou casada e tenho como propósito
+						compartilhar conhecimento, ajudando pessoas a se desenvolverem. Com
+						experiência em saúde ocupacional, ergonomia e bem-estar, ofereço
+						palestras personalizadas focadas no desenvolvimento pessoal e
+						profissional. Ensino técnicas para gerenciar o estresse e
+						transformar desafios em oportunidades, promovendo clareza e
+						automotivação.
+					</p>
+				</div>
 			</div>
-			<img
-				className={styles.profile}
-				src={ProfileImg}
-				alt="Foto de Laís"
-				draggable={false}
-			/>
 		</section>
 	);
 };

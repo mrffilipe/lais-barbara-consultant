a1cfe175b37484060cc6dd8a3fa0fe20f64aaba6
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2025-01-24 11:19:56 -0300

    Add adjusts and deploy

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -10,9 +10,8 @@ const Home = () => {
 					<div className={styles.content}>
 						<h1 className={styles.title}>Bem-vindo(a)!</h1>
 						<p className={styles.description}>
-							Ofereço palestras descontraídas para empresas, escolas, workshops
-							e seminários, com ferramentas essenciais para o desenvolvimento
-							pessoal e profissional.
+							Ofereço palestras, workshops e seminários com ferramentas
+							essenciais para o desenvolvimento pessoal e profissional.
 						</p>
 						<a href="#contact" className={styles.cta_button}>
 							Saiba Mais
